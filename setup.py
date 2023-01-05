773515a1825c16f100fae5fb7ed4bbbe3889e6b6
ThatcherK <kaliithatmau@gmail.com>
2023-01-05 16:16:14 +0300

    Add install requirement

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup, find_packages
 setup(name='slack_messages',
       version='0.1',
       packages=find_packages(),
+      install_requires=('slack_sdk>=3.13.0',),
       )

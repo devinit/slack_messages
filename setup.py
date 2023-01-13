b6dc177a7033ffa0034c77b8084f4cc047eb889d
ThatcherK <kaliithatmau@gmail.com>
2023-01-13 12:34:55 +0300

    Remove build files from tracking

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='slack_hero',
       version='0.2',
-      description='A package that enables you to easily exception messages from django to slack',
+      description='A package that enables you to easily send exception messages from django to slack',
       packages=find_packages(),
       python_requires=">=3.6",
       install_requires=('slack_sdk>=3.13.0',),

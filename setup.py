2b332af61f6aede1e617646490041ef333759858
ThatcherK <kaliithatmau@gmail.com>
2023-01-16 08:45:41 +0300

    Use both pyproject and setuptools

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from setuptools import setup, find_packages
+
+setup(name='slack_hero',
+      version='0.2.1',
+      packages=find_packages(),
+      python_requires=">=3.6",
+      install_requires=('slack_sdk>=3.13.0',),
+      url='https://github.com/devinit/slack_messages',
+      )
